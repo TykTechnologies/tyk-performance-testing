a55e708767158e11bc9776cf4656a1c9ed65eadf
Zaid Albirawi <zaid@tyk.io>
2023-11-02 17:57:11 -0400

    Remove k6 old chart

--- modules/deployments/dependencies/k6-operator.tf
+++ modules/deployments/dependencies/k6-operator.tf
@@ -1,6 +1,5 @@
 resource "helm_release" "k6-operator" {
   name       = "k6-operator"
-#  chart      = "../modules/deployments/dependencies/k6-operator/charts"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "k6-operator"
   version    = "1.2.0"
