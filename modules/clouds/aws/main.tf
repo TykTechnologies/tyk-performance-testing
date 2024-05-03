6d14bfbc04cb0179d81b3ca6aa3885a874a9d245
Zaid Albirawi <zaid@tyk.io>
2024-05-03 13:31:24 -0400

    Add create admin permissions to eks

--- modules/clouds/aws/main.tf
+++ modules/clouds/aws/main.tf
@@ -28,6 +28,7 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
 }
 
 module "eks_node_groups" {
