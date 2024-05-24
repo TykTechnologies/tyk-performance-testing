49b4068f0be5df3223cad54daa2a9dff54183a7c
Zaid Albirawi <zaid@tyk.io>
2024-05-24 19:38:52 -0400

    fix workflows

--- .github/custom-tests/tyk-open-telemetry/cluster.tf
+++ .github/custom-tests/tyk-open-telemetry/cluster.tf
@@ -1,10 +1,15 @@
+cluster_location          = "westus"
+aks_version               = "1.29"
+cluster_machine_type      = "Standard_F8s_v2"
+service_machine_type      = ""
+upstream_machine_type     = ""
+tests_machine_type        = ""
+resources_machine_type    = ""
+dependencies_machine_type = ""
+
 services_nodes_count     = 1
 resource_nodes_count     = 1
 dependencies_nodes_count = 2
-
-cluster_location     = "westus"
-cluster_machine_type = "Standard_F8s_v2"
-aks_version          = "1.29"
 
 tyk_enabled      = true
 kong_enabled     = false
