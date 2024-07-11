dc96eb22cd4e7b8e6c6144d111fb84c7dc5c1018
Zaid Albirawi <zaid@tyk.io>
2024-07-11 10:28:21 -0400

    Add upstream and tests node counts

--- modules/helpers/output.tf
+++ modules/helpers/output.tf
@@ -2,16 +2,16 @@ locals {
   nodes = {
     dependencies       = var.dependencies_nodes_count
     tyk                = var.tyk_enabled      ? var.services_nodes_count : 0
-    tyk-upstream       = var.tyk_enabled      ? var.services_nodes_count : 0
-    tyk-tests          = var.tyk_enabled      ? var.services_nodes_count : 0
+    tyk-upstream       = var.tyk_enabled      ? var.upstream_nodes_count : 0
+    tyk-tests          = var.tyk_enabled      ? var.tests_nodes_count    : 0
     tyk-resources      = var.tyk_enabled      ? var.resource_nodes_count : 0
     kong               = var.kong_enabled     ? var.services_nodes_count : 0
-    kong-upstream      = var.kong_enabled     ? var.services_nodes_count : 0
-    kong-tests         = var.kong_enabled     ? var.services_nodes_count : 0
+    kong-upstream      = var.kong_enabled     ? var.upstream_nodes_count : 0
+    kong-tests         = var.kong_enabled     ? var.tests_nodes_count    : 0
     kong-resources     = var.kong_enabled     ? var.resource_nodes_count : 0
     gravitee           = var.gravitee_enabled ? var.services_nodes_count : 0
-    gravitee-upstream  = var.gravitee_enabled ? var.services_nodes_count : 0
-    gravitee-tests     = var.gravitee_enabled ? var.services_nodes_count : 0
+    gravitee-upstream  = var.gravitee_enabled ? var.upstream_nodes_count : 0
+    gravitee-tests     = var.gravitee_enabled ? var.tests_nodes_count    : 0
     gravitee-resources = var.gravitee_enabled ? var.resource_nodes_count : 0
   }
 
