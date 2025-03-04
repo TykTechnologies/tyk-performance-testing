010966742cf5cf4a26849d55ee1d0a85f4ffa19f
Ahmet Soormally <ahmet@mangomm.co.uk>
2025-03-04 13:34:30 +0000

    reducing the test time to 5m

--- .github/custom-tests/comparison/all/tests.tf
+++ .github/custom-tests/comparison/all/tests.tf
@@ -6,7 +6,7 @@ tests_fortio_options = "size=2000"
 tests_executor       = "constant-arrival-rate"
 tests_auth_key_count = 10
 tests_ramping_steps  = 10
-tests_duration       = 120
+tests_duration       = 5
 tests_rate           = 5000
 tests_virtual_users  = 500
 tests_parallelism    = 4
